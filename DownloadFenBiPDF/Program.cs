b4da18fa7a8b63f620ace6b6d2c468240a47c600
frisktale <frisktale@live.com>
2021-09-21 21:29:54 +0800

    优化代码 利用多线程加速下载。

--- DownloadFenBiPDF/Program.cs
+++ DownloadFenBiPDF/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Net.Http;
@@ -10,9 +11,9 @@ using static System.Net.Mime.MediaTypeNames;
 
 namespace DownloadFenBiPDF
 {
-    internal class Program
+    public class Program
     {
-        static async Task Main(string[] args)
+        public static async Task Main(string[] args)
         {
 
             var configurationBuilder = new ConfigurationBuilder();
@@ -28,42 +29,43 @@ namespace DownloadFenBiPDF
             using var client = new HttpClient();
             client.DefaultRequestHeaders.Add("Cookie", cookie);
 
+            var taskList = new List<Task>();
 
             for (int i = 0; i < count; i++)
             {
-                var id = await GetPageIdAsync(client);
-                await DownloadPDFAsync(client, id, i);
+                taskList.Add(DownloadRandomPDF(client, i));
             }
 
+            await Task.WhenAll(taskList);
         }
+
+        private static async Task DownloadRandomPDF(HttpClient client, int count)
+        {
+            var id = await GetPageIdAsync(client);
+            await DownloadPDFAsync(client, id, count);
+        }
+
         /// <summary>
         /// 获取一个智能组卷的id
         /// </summary>
         /// <param name="client">复用httpclient</param>
         /// <returns>智能组卷id</returns>
-        private static async Task<string> GetPageIdAsync(HttpClient client)
+        private static async Task<int> GetPageIdAsync(HttpClient client)
         {
-            var request = new HttpRequestMessage
-            {
-                RequestUri = new Uri("https://tiku.fenbi.com/api/sydw/exercises?app=web&kav=12&version=3.0.0.0"),
-                Method = HttpMethod.Post
-            };
-
-
             var content = new MultipartFormDataContent
             {
                 { new StringContent("2"), "type" },
                 { new StringContent("2"), "exerciseTimeMode" }
             };
-            request.Content = content;
 
-            var response = await client.SendAsync(request);
+            var response = await client.PostAsync("https://tiku.fenbi.com/api/sydw/exercises?app=web&kav=12&version=3.0.0.0",content);
             var result = await response.Content.ReadAsStringAsync();
 
             using JsonDocument jsonDocument = JsonDocument.Parse(result);
             var id = jsonDocument.RootElement.GetProperty("id");
 
-            return id.GetInt32().ToString();
+            //System.Text.Json很严格，是数字类型就不能获取为字符串
+            return id.GetInt32();
 
         }
 
@@ -74,19 +76,13 @@ namespace DownloadFenBiPDF
         /// <param name="id">智能组卷的id</param>
         /// <param name="fileCount">当前循环次数，用于给下载的pdf编号</param>
         /// <returns></returns>
-        private static async Task DownloadPDFAsync(HttpClient client,string id,int fileCount)
+        private static async Task DownloadPDFAsync(HttpClient client,int id,int fileCount)
         {
             string downloadUrl = $"https://urlimg.fenbi.com/api/pdf/tiku/sydw/exercise/{id}?app=web&kav=12&version=3.0.0.0";
             //因为是pdf文件，所以不能用GetStream而应该用GetByte
             var bytes = (await client.GetByteArrayAsync(downloadUrl));
             var fileName = $"试卷{DateTime.Now:MM-dd}-{fileCount}.pdf";
-            var file = new FileInfo(fileName);
-            if (file.Exists)
-            {
-                file.Delete();
-            }
-            using var stream = file.Open(FileMode.Create);
-            //using var stream = new FileStream(fileName, FileMode.Create);
+            using var stream = new FileStream(fileName, FileMode.Create);
             //写入内存中的byte[]建议用这种方法，byte[]直接转成ReadOnlyMemory<byte>
             await stream.WriteAsync(bytes);
         }
